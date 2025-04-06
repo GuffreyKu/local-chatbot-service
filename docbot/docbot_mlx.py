cf5336dc71bf52d54dedd6d2a14de499e1cd8d7c
Guffrey <guffrey@GuffreydeMacBook-Pro.local>
2025-04-06 20:21:28 +0800

    fix: use langchain MLXPipeline

--- docbot/docbot_mlx.py
+++ docbot/docbot_mlx.py
@@ -1,45 +1,22 @@
 import os
 os.environ['KMP_DUPLICATE_LIB_OK']='True'
-import requests
-from pydantic import Field
-from typing import List, Optional
-from langchain.llms.base import LLM  # Use the base LLM from langchain.llms.base
+from langchain_community.llms.mlx_pipeline import MLXPipeline
 from langgraph.checkpoint.memory import MemorySaver
 from graph.flow import Graph
 from graph.utils.pdf_embedding import multi_pdf_embedding
 
-class LocalLLM(LLM):
-    url: str = Field(..., description="Local LLM endpoint URL")
-    temperature: float = Field(1, description="Sampling temperature")
 
-    @property
-    def _llm_type(self) -> str:
-        return "local_llm"
-
-    def _call(self, prompt: str, stop: Optional[List[str]] = None) -> str:
-        data = {
-            "messages": [{"role": "user", "content": prompt}],
-            "temperature": self.temperature
-        }
-        response = requests.post(self.url, json=data)
-        
-        if response.status_code != 200:
-            raise Exception(f"Request failed with status {response.status_code}: {response.text}")
-        response_json = response.json()
-        # print(response_json)
-        # Adjust the key based on your API's response structure.
-        return response_json.get("choices")[0].get("message").get("content")
-
-# Initialize the local LLM using your endpoint
-local_llm = LocalLLM(url="http://localhost:8080/v1/chat/completions", temperature=1)
-
+llm = MLXPipeline.from_model_id(
+    "mlx-community/DeepSeek-R1-Distill-Qwen-32B-4Bit",
+    pipeline_kwargs={"max_tokens": 2048, "temp": 1.0, "verbose":True},
+)
 
 pdf_list = ["../data/pdf/"+pdf_path for pdf_path in os.listdir("../data/pdf/")]
 
 vector_store = multi_pdf_embedding(pdf_list)
 
 config = {"configurable": {"thread_id": "1",
-                           "model": local_llm,
+                           "model": llm,
                            "pdf_vector": vector_store}
                            }
 
